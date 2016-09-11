9b15c89a7f1133835cf3689644d4a9ee92d91264
Bruno Konrad <bruno.skon@gmail.com>
2016-09-11 18:26:38 -0300

    Rename android package to `com.brunokonrad.userapp`

--- android/app/src/main/java/com/brunokonrad/usersapp/MainActivity.java
+++ android/app/src/main/java/com/brunokonrad/usersapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.usersapp;
+package com.brunokonrad.usersapp;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/brunokonrad/usersapp/MainApplication.java
+++ android/app/src/main/java/com/brunokonrad/usersapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.usersapp;
+package com.brunokonrad.usersapp;
 
 import android.app.Application;
 import android.util.Log;
